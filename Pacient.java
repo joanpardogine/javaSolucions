9b40bfd94e2b4ab41d699500061e7818f1710a9d
joanpardogine <joanpardo@ginebro.cat>
2023-05-08 19:54:51 +0200

    Creat Pacient.java i actulitzat  Persona.java!

---
+++ Pacient.java
@@ -0,0 +1,21 @@
+import java.util.Calendar;
+
+public class Pacient extends Persona {
+    // Atributs
+    private String malaltia;
+    private int visites;
+
+    // Constructor
+    public Pacient(String nomRebut, String cognomRebut, String telefonRebut, String correuRebut, Calendar dataNaixamentRebuda, String malaltiaRebuda, int visitesRebudes ){
+        super(nomRebut, cognomRebut, telefonRebut, correuRebut,  dataNaixamentRebuda);
+        this.malaltia = malaltiaRebuda;
+        this.visites = visitesRebudes;
+    }
+    // Mètodes
+    public void aumentarVisites(){
+        this.visites ++;
+    }
+
+    // Getters
+
+}
