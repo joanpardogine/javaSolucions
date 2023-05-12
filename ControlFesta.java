2a85013f97ad132b0f5e7564e7621f92a937931f
joanpardogine <joanpardo@ginebro.cat>
2023-05-12 15:13:34 +0200

    Pujat fitxer ControlFesta.java!

---
+++ ControlFesta.java
@@ -0,0 +1,15 @@
+public class ControlFesta {
+    // A una festa assisteixen persones de diferents edats i sexe. Desenvolupa programa en java que,
+    // donades les edats i el sexe (HOME-DONA-ALTRE) de les persones assistents, calculi:
+    // *) Nombre total de persones que assisteixen a la festa.
+    // *) Nombre total d'homes, dones i altres.
+    // *) Mitjana de les edats per sexe.
+    // *) Edat de la persona més jove i la més vella.
+    // S'ha de tenir en compte que no es permet l'entrada a menors d'edat i s'ha de controlar que
+    // l'usuari s'equivoca i introdueixi edats negatives, li aparegui un missatge d'error.
+    // El programa anirà demant dades fins que es llegeixi una edat igual a 0.
+    // Cal fer servir una constant amb el nom MAJOR_EDAT.
+
+
+    
+}
