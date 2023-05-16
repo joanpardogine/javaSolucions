b42831741fa21c8aa2e449d377d9fa91e2b09719
joanpardogine <joanpardo@ginebro.cat>
2023-05-16 16:48:16 +0200

    Afegit LongitudCadenes.java!

---
+++ LongitudCadenes.java
@@ -0,0 +1,19 @@
+// import java.util.Scanner;
+
+public class LongitudCadenes {
+ public static void main(String[] args) {
+    // Scanner teclat = new Scanner(System.in);
+
+String[] vLlegit = {"En un lugar de la Mancha",
+        "de cuyo nombre no quiero acordarme",
+        "no ha mucho tiempo que vivía un hidalgo",
+        "de los de lanza en astillero",
+        "adarga antigua, rocín flaco y galgo corredor"};
+
+    for (String frase : vLlegit) {
+        System.out.println(frase);
+        
+
+    }
+ }   
+}
